32fdd41d67de73b70ec73b537dc881ce032f14f1
lumilo8 <llopez@sopinet.com>
2015-07-01 13:20:23 +0200

    Checking response of travis

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,8 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
+        var_dump($client->getResponse()->getStatusCode());
+        var_dump($client->getResponse()->getContent());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('html:contains("Homepage.")')->count() > 0);
     }
