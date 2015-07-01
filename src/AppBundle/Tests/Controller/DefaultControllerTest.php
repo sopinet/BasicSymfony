5925c1a940d722b60447e43b7bae81c54cd12f92
lumilo8 <llopez@sopinet.com>
2015-07-01 12:51:16 +0200

    Fix defaultTest

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/app/example');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Homepage.")')->count() > 0);
     }
 }
