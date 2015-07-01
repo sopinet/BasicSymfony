00156a820f7d790e707bb494815589f3f4ae26b9
lumilo8 <llopez@sopinet.com>
2015-07-01 13:27:06 +0200

    Testing without var_dump

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -11,9 +11,7 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
-        var_dump($client->getResponse()->getStatusCode());
-        var_dump($client->getResponse()->getContent());
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('html:contains("Homepage.")')->count() > 0);
     }
